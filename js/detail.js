be3f3196fbe0e055d0270fe0b0a879e3bc96b57d
Zeno Zeng <zenoes@qq.com>
2013-05-13 20:00:29 +0800

    update data data/lianlian js/detail.js

--- js/detail.js
+++ js/detail.js
@@ -7,7 +7,7 @@ if(ua.indexOf('irefox') != -1) {
 var data = {
     lianlian: {
 	name: "连昱澎",
-        img: 3,
+        img: 5,
 	summary: '也许每个人都<br>会有另一个自己<br>另一个自己<br>有另一个不解的身世<br>也许有一天<br>就会遇见他',
         music: ["Play", "Siberia", "Wilderness II", "夜访组曲1-回温"],
 	video: '',
